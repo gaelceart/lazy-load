f08fd6017139de08258303eecdfeb08c2ceacf61
GaelCeart <gaelceart@gmail.com>
2022-02-08 11:29:56 -0300

    Add load in for loop, Change styles

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ const createPostCard = (author, post) => {
     const section = document.createElement("SECTION");  
     const name = document.createElement("H2");
     const content = document.createElement("DIV");
+    const commentSection = document.createElement("DIV");
+    const comment = document.createElement("INPUT");
+    const send = document.createElement("INPUT");
 
+    comment.setAttribute("type","text");
+    send.setAttribute("type","submit");
+    send.setAttribute("value","Comment");
+
+    commentSection.classList.add("comment-section");
+    comment.classList.add("comment-box");
+    send.classList.add("comment-send");
+    
     section.classList.add("post-card");
     name.classList.add("post-author")
     content.classList.add("post-content");
@@ -15,15 +26,26 @@ const createPostCard = (author, post) => {
     content.textContent = post;
     section.appendChild(name);
     section.appendChild(content);
+
+
+    commentSection.appendChild(comment);
+    commentSection.appendChild(send);
+
+    section.appendChild(commentSection);
     postCard.appendChild(section);
-    mainContainer.appendChild(postCard);
+    return postCard;
 }
 
-const requestData = async () =>{
+const requestData = async (num) =>{
     const request = await fetch("posts.txt");
     const content = await request.json();
-    createPostCard(content.name, content.content);
-    console.log(content);
+    // for(let i=0; i< num; i++) {
+        
+    // }
+    const newCards = new DocumentFragment;
+    for (let i = 0; i < num; i++){
+        newCards.appendChild(createPostCard(content[i].name, content[i].content))
+    }
+    mainContainer.appendChild(newCards)
 }
-
-requestData()
+requestData(3)
